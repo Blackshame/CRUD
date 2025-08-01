e1d1747366c896b35b7eb33729690e5f3dfb5971
ransongyin <ransongyin@codemao.cn>
2025-08-01 20:37:30 +0800

    @RequestBody&log

--- src/main/java/com/example/crud/controller/StudentController.java
+++ src/main/java/com/example/crud/controller/StudentController.java
@@ -2,10 +2,12 @@ package com.example.crud.controller;
 import com.example.crud.entity.Result;
 import com.example.crud.entity.Student;
 import com.example.crud.service.StudentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+@Slf4j
 
 @RestController
 @RequestMapping(value = "/student")
@@ -16,8 +18,7 @@ public class StudentController {
     //添加新的学生信息
     @PostMapping
     public Result addStudent(@RequestBody Student student) {
-        System.out.println("接收的 JSON 数据：" + student);
-
+        log.info("【添加学生】请求体 student: {}", student);
         String studentid = student.getStudentid();
         if (studentService.getStudent(studentid) != null) {
             return Result.error("学号已存在");
@@ -30,10 +31,11 @@ public class StudentController {
     //更新学生信息
     @PutMapping("/{studentid}")
     public Result updateStudent(@PathVariable String studentid, @RequestBody Student student) {
-        System.out.println("路径中的 studentid = " + studentid);
-        System.out.println("请求体中的 student = " + student);
+        log.info("【更新学生】路径 studentid: {}", studentid);
+        log.info("【更新学生】请求体 student: {}", student);
         // 强制将路径中的 id 设置到 student 对象，确保一致
         student.setStudentid(studentid);
+        log.info("【更新前 student 设置后】：{}", student);
         if (studentService.getStudent(studentid) == null) {
             return Result.error("找不到该学生");
         }
@@ -46,6 +48,7 @@ public class StudentController {
     //删除学生信息
     @DeleteMapping("/{studentid}")
     public Result deleteStudent(@PathVariable String studentid) {
+        log.info("【删除学生】studentid: {}", studentid);
         Student student = studentService.getStudent(studentid);
         if(student == null){
             return Result.error("学号不存在");
@@ -59,6 +62,7 @@ public class StudentController {
     //根据学号查询学生信息
     @GetMapping("/{studentid}")
     public Result getStudent(@PathVariable String studentid) {
+        log.info("【查询学生】studentid: {}", studentid);
         Student student = studentService.getStudent(studentid);
         if(student == null){
             return Result.error("对应学生不存在");
@@ -71,6 +75,7 @@ public class StudentController {
     //查询所有学生的信息
     @GetMapping
     public Result getAllStudent() {
+        log.info("【查询全部学生】");
         List<Student> students = studentService.getAllStudents();
         return Result.success("查询成功",students);
     }
